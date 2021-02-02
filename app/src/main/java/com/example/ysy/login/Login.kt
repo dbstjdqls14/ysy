be5770490afa66fa7f6ca84bfc449d5006a2a274
seungyong <44765636+seungyong@users.noreply.github.com>
2021-02-02 21:15:32 +0900

    프로젝트 구조 변경, 설정

---
+++ app/src/main/java/com/example/ysy/login/Login.kt
@@ -0,0 +1,13 @@
+package com.example.ysy.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ysy.R
+
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
